880ef8d8208cbacd830258cd23390f45dd2934e7
costaaaj <miguelcosta220@hotmail.com>
2023-11-19 15:01:57 +0000

    update

--- routes.js
+++ routes.js
@@ -6,23 +6,27 @@ router.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+router.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
 
+router.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
 
 
 router.post('/register', async (req, res) => {
   const { username, password } = req.body;
   const db = req.db;
 
-  // Check if the user already exists
   const existingUser = await db.collection('users').findOne({ username });
 
   if (existingUser) {
     return res.status(409).json({ message: 'User already exists' });
   }
 
-  // Insert the new user
   const result = await db.collection('users').insertOne({ username, password });
-  res.status(201).json({ message: 'User created', userId: result.insertedId });
+  //res.status(201).json({ message: 'User created', userId: result.insertedId });
 });
 
 
@@ -38,7 +42,8 @@ router.post('/login', async (req, res) => {
     return res.status(401).json({ message: 'Invalid credentials' });
   }
 
-  res.json({ message: 'Login successful', user });
+  //res.json({ message: 'Login successful', user });
+  return res.sendFile(path.join(__dirname, 'public', 'menu.html'));
 });
 
 
--- server.js
+++ server.js
@@ -4,31 +4,33 @@ const routes = require('./routes');
 const port = process.env.PORT || 8080;
 const path = require('path'); 
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 const mongoUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017';
 const dbName = process.env.DB_NAME || 'my_db';
 
-MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-  if (err) {
+MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db(dbName);
+
+    // Pass the MongoDB database connection to the routes
+    app.use((req, res, next) => {
+      req.db = db;
+      next();
+    });
+
+    // Set up routes
+    app.use(express.json());
+    app.use('/', routes);
+    app.use(express.static('public'));
+
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
-    return;
-  }
-
-  const db = client.db(dbName);
-
-  app.use((req, res, next) => {
-    req.db = db;
-    next();
   });
-
-  app.use(express.json());
-  app.use('/', routes);
-  app.use(express.static('public'));
-
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
-
-  
-});
-
