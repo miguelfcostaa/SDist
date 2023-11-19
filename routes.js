05ee2ae538e3a63dd45c1f59ce44ae139396785e
costaaaj <miguelcosta220@hotmail.com>
2023-11-19 14:01:06 +0000

    MongoDB no codigo

--- routes.js
+++ routes.js
@@ -6,12 +6,40 @@ router.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-router.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+
+
+
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const db = req.db;
+
+  // Check if the user already exists
+  const existingUser = await db.collection('users').findOne({ username });
+
+  if (existingUser) {
+    return res.status(409).json({ message: 'User already exists' });
+  }
+
+  // Insert the new user
+  const result = await db.collection('users').insertOne({ username, password });
+  res.status(201).json({ message: 'User created', userId: result.insertedId });
 });
 
-router.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'register.html'));
+
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const db = req.db;
+
+  // Check if the user exists and the password is correct
+  const user = await db.collection('users').findOne({ username, password });
+
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+
+  res.json({ message: 'Login successful', user });
 });
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,18 +3,32 @@ const app = express();
 const routes = require('./routes');
 const port = process.env.PORT || 8080;
 const path = require('path'); 
+const MongoClient = require('mongodb').MongoClient;
 
+const mongoUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017';
+const dbName = process.env.DB_NAME || 'my_db';
 
-app.use(express.json());
+MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+    return;
+  }
 
-app.use('/', routes);
+  const db = client.db(dbName);
 
-app.use(express.static('public'));
+  app.use((req, res, next) => {
+    req.db = db;
+    next();
+  });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  app.use(express.json());
+  app.use('/', routes);
+  app.use(express.static('public'));
+
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+
+  
 });
-
-
-
 
